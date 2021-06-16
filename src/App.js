015c04cef6148e4d5f83a11daa30729476757d18
just-noname-bing <danikfedorov41@gmail.com>
2021-06-17 01:36:54 +0300

    fixed mobile view and fixed 2 errors

--- src/App.js
+++ src/App.js
@@ -5,74 +5,76 @@ import Weather from "./components/weather";
 
 const API_KEY = "4243f663d86eabbb1ceb89a91a02d221";
 
-class App extends React.Component{
+const stateDefault = {
+    temp: undefined,
+    city: undefined,
+    country: undefined,
+    pressure: undefined,
+    sunset: undefined,
+    error: undefined,
+};
 
-    state = {
-        temp: undefined,
-        city: undefined,
-        country: undefined,
-        pressure: undefined,
-        sunset: undefined,
-        error: undefined
-    }
+class App extends React.Component {
+    state = stateDefault;
 
     gettingWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
 
-        if(city){
-            const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+        if (city) {
+            const api_url = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+            );
             const data = await api_url.json();
 
-            var sunset = data.sys.sunset;
-            var date = new Date();
-            date.setTime(sunset);
-            var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+            try {
+                var sunset = data.sys.sunset;
+                var date = new Date();
+                date.setTime(sunset);
+                var sunset_date =
+                    date.getHours() +
+                    ":" +
+                    date.getMinutes() +
+                    ":" +
+                    date.getSeconds();
 
+                this.setState({
+                    temp: data.main.temp,
+                    city: data.name,
+                    country: data.sys.country,
+                    pressure: data.main.pressure,
+                    sunset: sunset_date,
+                    error: undefined,
+                });
+            } catch (error) {
+                this.setState({
+                    ...stateDefault,
+                    error: "Ведите название города",
+                });
+            }
+        } else {
             this.setState({
-                temp: data.main.temp,
-                city: data.name,
-                country: data.sys.country,
-                pressure: data.main.pressure,
-                sunset: sunset_date,
-                error: undefined
-            });
-        }else{
-            this.setState({
-                temp: undefined,
-                city: undefined,
-                country: undefined,
-                pressure: undefined,
-                sunset: undefined,
-                error: "Ведите название города"
+                ...stateDefault,
+                error: "Ведите название города",
             });
         }
-    }
+    };
 
-    render(){
-        return(
-            <div className="wrapper">
-                <div className="main">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-sm-5 info">
-                                <Info />
-                            </div>
-                            <div className="col-sm-7 form">
-                                <Form weatherMethod={this.gettingWeather} />
-                                <Weather 
-                                    temp={this.state.temp}
-                                    city={this.state.city}
-                                    country={this.state.country}
-                                    pressure={this.state.pressure}
-                                    sunset={this.state.sunset}
-                                    error={this.state.error}
-                                />
-                            </div>
+    render() {
+        return (
+            <div className="main-content">
+                <div className="items">
+                    <div className="hero info">
+                        <Info />
+                    </div>
+                    <div className="content form">
+                        <Form weatherMethod={this.gettingWeather} />
+                        <div>
+                            <Weather {...this.state} />
                         </div>
                     </div>
                 </div>
-            </div>    
+            </div>
         );
     }
 }
